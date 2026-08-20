d79c32b1ea4827c1204157d3c33cebd30360d358
Tony Ghandour <tghandour@multilanetestproducts.com>
2026-08-20 11:44:53 +0300

    Add project files.

---
+++ OldPhoneKeypad/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+class Program
+{
+    public static Dictionary<char, string> digitMapping = new Dictionary<char, string>()
+    {
+        {'1', "&'("}, {'2', "abc"},
+        {'3', "def"}, {'4', "ghi"},
+        {'5', "jkl"}, {'6', "mno"},
+        {'7', "pqrs"}, {'8', "tuv"},
+        {'9', "wxyz"}, {'0', " "}
+    };
+
+    public static String GetOldPhoneNumber(string input)
+    {
+        string ans = ""; int i = 0;
+        while (i < input.Length)
+        {
+            if ((input[i] < '0' || input[i] > '9') && input[i] != '*' && input[i] != '#' && input[i] != ' ') throw new Exception("Invalid Format");
+            i++;
+        }
+        i = 0;
+        while ((input[i] < '0' || input[i] > '9') && input[i] != '#') i++;
+        if (input[i] == '#')
+            return ans;
+        while (i < input.Length)
+        {
+            int j = 0;
+            char currentChar = input[i];
+            char currentValidChar = ' ';
+            while (i < input.Length && currentChar >= '1' && currentChar <= '9' && currentChar == input[i])
+            {
+                currentValidChar = digitMapping[input[i]][j];
+                j = (j + 1) % digitMapping[input[i]].Length;
+                i++;
+            }
+            if (currentChar == '*')
+                ans = ans.Substring(0, ans.Length == 0 ? 0 : ans.Length - 1);
+            else if (digitMapping.TryGetValue(currentChar, out var value))
+                ans += currentValidChar;
+            if (currentChar < '1' || currentChar > '9') i++;
+            if (currentChar == '#') break;
+        }
+        return ans;
+    }
+
+    public static void Main(string[] args)
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            Console.WriteLine("Enter the number: ");
+            string input = Console.ReadLine();
+            string output = GetOldPhoneNumber(input);
+            Console.WriteLine("output: " + output);
+        }
+    }
+}
